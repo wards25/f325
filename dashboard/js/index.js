a8db857aab68354dea4540255456b28e593d1bde
RGCIT3 <edwardjohndelbarrio92@gmail.com>
2025-11-03 10:06:33 +0800

    deobfuscate javascript

--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -114,7 +114,7 @@ function UpdateCompany() {
     type: "POST",
     url: "company/updatecompany.php",
     data: {
-      id:  id, 
+      id: id,
       name: companyname,
       nickname: nickname,
       vendorcode: vendorcode,
@@ -124,12 +124,12 @@ function UpdateCompany() {
       active: isActive,
     },
     success: function () {
-      $("#updateMessage").text("Company updated successfully!");
-      $("#updateMessage").show();
-      $("#companyForm input").val("");
-      $("#companyForm").css("background-Color", "");
+      $(".span-notify-alert").text("Company updated successfully!");
+      $(".span-notify-alert").show();
+      $(".form-company").css("background-Color", "");
+      $(".div-load-data").html("");
       setTimeout(function () {
-        $("#updateMessage").hide();
+        $(".span-notify-alert").fadeOut();
       }, 2000);
     },
   });
@@ -200,239 +200,232 @@ function OnsubmitUserValue() {
 }
 
 function NewUser() {
-  var _0x3f3eda = _0x143b0a,
-    _0x67d92f = $(_0x3f3eda(0x113))[_0x3f3eda(0x141)](),
-    _0x2753ee = $(_0x3f3eda(0x14f))[_0x3f3eda(0x141)](),
-    _0x53b296 = $(_0x3f3eda(0x119))[_0x3f3eda(0x141)](),
-    _0x2fc36e = $(_0x3f3eda(0x1bc))[_0x3f3eda(0x141)](),
-    _0x2b9a2d = $(_0x3f3eda(0x16c))[_0x3f3eda(0x141)](),
-    _0x15fbdd = $(_0x3f3eda(0x11a))["val"](),
-    _0x1f6b04 = $(_0x3f3eda(0x177))["val"](),
-    _0x4bf836 = $(".input-comp3:checked")[_0x3f3eda(0x141)](),
-    _0x245edf = $(_0x3f3eda(0x110))[_0x3f3eda(0x141)](),
-    _0x34a717 = $(".input-comp5:checked")[_0x3f3eda(0x141)](),
-    _0x1d7d71 = $(_0x3f3eda(0x17b))[_0x3f3eda(0x141)](),
-    _0xae3741 = $(_0x3f3eda(0x136))["val"](),
-    _0x877d03 = $(_0x3f3eda(0x161))["val"](),
-    _0x352bea = $(_0x3f3eda(0x13f))[_0x3f3eda(0x141)](),
-    _0x91110 = $(_0x3f3eda(0x14c))["val"](),
-    _0x546c3b = $(_0x3f3eda(0x131))[_0x3f3eda(0x141)](),
-    _0x14ee42 = $(_0x3f3eda(0x146))[_0x3f3eda(0x141)](),
-    _0x348b81 = $(".input-loc3:checked")[_0x3f3eda(0x141)](),
-    _0x2ed64c = $(".input-loc4:checked")[_0x3f3eda(0x141)](),
-    _0x5d60cc = $(_0x3f3eda(0x17c))[_0x3f3eda(0x141)](),
-    _0x214db9 = $(_0x3f3eda(0x18f))["val"](),
-    _0x5c6467 = $(_0x3f3eda(0x117))[_0x3f3eda(0x141)](),
-    _0x3d7852 = $(_0x3f3eda(0x10d))["val"](),
-    _0x1da5c0 = $(".input-loc9:checked")[_0x3f3eda(0x141)](),
-    _0x92b160 = $(_0x3f3eda(0x15b))[_0x3f3eda(0x141)](),
-    _0x268b89 = $(_0x3f3eda(0x15f))[_0x3f3eda(0x141)](),
-    _0x56c320 = $(_0x3f3eda(0x1ae))[_0x3f3eda(0x141)](),
-    _0x191a2a = $(_0x3f3eda(0x14d))[_0x3f3eda(0x141)](),
-    _0x3d893c = $(_0x3f3eda(0x19d))["val"](),
-    _0x19ffb7 = $(_0x3f3eda(0x172))["val"](),
-    _0x5d2705 = $(_0x3f3eda(0x1a0))[_0x3f3eda(0x141)](),
-    _0x31f420 = $(_0x3f3eda(0x157))["val"](),
-    _0xa8bac = $(_0x3f3eda(0x19b))["val"](),
-    _0x30f390 = $(_0x3f3eda(0x19a))[_0x3f3eda(0x141)](),
-    _0x28343c = $(_0x3f3eda(0x1c2))[_0x3f3eda(0x141)](),
-    _0x605022 = $(_0x3f3eda(0x1a3))[_0x3f3eda(0x141)](),
-    _0x5db172 = $(".input-deduction:checked")[_0x3f3eda(0x141)](),
-    _0x4cf95b = $(_0x3f3eda(0x171))["val"](),
-    _0x57d614 = $(_0x3f3eda(0x180))["val"](),
-    _0x17a1e4 = $(_0x3f3eda(0x11c))[_0x3f3eda(0x141)](),
-    _0x4803e5 = $(_0x3f3eda(0x147))[_0x3f3eda(0x141)](),
-    _0x17fe20 = $(".input-pulloutdoc:checked")[_0x3f3eda(0x141)](),
-    _0x1cc07b = $(_0x3f3eda(0x13a))[_0x3f3eda(0x141)](),
-    _0xd1ac70 = $(".input-syssetting:checked")[_0x3f3eda(0x141)](),
-    _0x565a9e = $(".input-active:checked")[_0x3f3eda(0x141)]();
-  return (
-    $[_0x3f3eda(0x168)]({
-      type: "POST",
-      beforeSend: function () {
-        var _0x1e6ab = _0x3f3eda;
-        $(_0x1e6ab(0x188))[_0x1e6ab(0x193)](_0x1e6ab(0x139), !![]),
-          $(".input-username")[_0x1e6ab(0x193)](_0x1e6ab(0x139), !![]),
-          $(".input-password")[_0x1e6ab(0x193)](_0x1e6ab(0x139), !![]),
-          $(_0x1e6ab(0x150))[_0x1e6ab(0x193)]("disabled", !![]);
-      },
-      url: _0x3f3eda(0x170),
-      data: {
-        username: _0x67d92f,
-        password: _0x2753ee,
-        fname: _0x53b296,
-        admin: _0x2fc36e,
-        semiadmin: _0x2b9a2d,
-        comp1: _0x15fbdd,
-        comp2: _0x1f6b04,
-        comp3: _0x4bf836,
-        comp4: _0x245edf,
-        comp5: _0x34a717,
-        comp6: _0x1d7d71,
-        comp7: _0xae3741,
-        comp8: _0x877d03,
-        comp9: _0x352bea,
-        comp10: _0x91110,
-        loc1: _0x546c3b,
-        loc2: _0x14ee42,
-        loc3: _0x348b81,
-        loc4: _0x2ed64c,
-        loc5: _0x5d60cc,
-        loc6: _0x214db9,
-        loc7: _0x5c6467,
-        loc8: _0x3d7852,
-        loc9: _0x1da5c0,
-        loc10: _0x92b160,
-        store: _0x268b89,
-        inventory: _0x56c320,
-        upload: _0x191a2a,
-        importdop: _0x3d893c,
-        print: _0x19ffb7,
-        schedule: _0x5d2705,
-        clearing: _0x31f420,
-        manual: _0xa8bac,
-        fordeduct: _0x30f390,
-        borfapps: _0x28343c,
-        dmpiraw: _0x605022,
-        deduction: _0x5db172,
-        deductdoc: _0x4cf95b,
-        paiddeduction: _0x57d614,
-        payment: _0x17a1e4,
-        rts: _0x4803e5,
-        pulloutdoc: _0x17fe20,
-        report: _0x1cc07b,
-        system: _0xd1ac70,
-        active: _0x565a9e,
-      },
-      success: function () {
-        var _0x75767d = _0x3f3eda;
-        $(_0x75767d(0x179))[_0x75767d(0x149)](_0x75767d(0x142)),
-          $(_0x75767d(0x179))[_0x75767d(0x156)](),
-          setTimeout(function () {
-            var _0x263d7a = _0x75767d;
-            $(_0x263d7a(0x1b0))[_0x263d7a(0x1a4)](_0x263d7a(0x1c0)),
-              $(_0x263d7a(0x179))[_0x263d7a(0x126)]();
-          }, 0x7d0);
-      },
-    }),
-    ![]
-  );
+  var username = $(".input-username").val();
+  var password = $(".input-password").val();
+  var fname = $(".input-fname").val();
+  var admin = $(".input-admin:checked").val();
+  var semiadmin = $(".input-semiadmin:checked").val();
+  var comp1 = $(".input-comp1:checked").val();
+  var comp2 = $(".input-comp2:checked").val();
+  var comp3 = $(".input-comp3:checked").val();
+  var comp4 = $(".input-comp4:checked").val();
+  var comp5 = $(".input-comp5:checked").val();
+  var comp6 = $(".input-comp6:checked").val();
+  var comp7 = $(".input-comp7:checked").val();
+  var comp8 = $(".input-comp8:checked").val();
+  var comp9 = $(".input-comp9:checked").val();
+  var comp10 = $(".input-comp10:checked").val();
+  var loc1 = $(".input-loc1:checked").val();
+  var loc2 = $(".input-loc2:checked").val();
+  var loc3 = $(".input-loc3:checked").val();
+  var loc4 = $(".input-loc4:checked").val();
+  var loc5 = $(".input-loc5:checked").val();
+  var loc6 = $(".input-loc6:checked").val();
+  var loc7 = $(".input-loc7:checked").val();
+  var loc8 = $(".input-loc8:checked").val();
+  var loc9 = $(".input-loc9:checked").val();
+  var loc10 = $(".input-loc10:checked").val();
+  var store = $(".input-store:checked").val();
+  var inventory = $(".input-inventory:checked").val();
+  var upload = $(".input-import:checked").val();
+  var importdop = $(".input-importdop:checked").val();
+  var print = $(".input-print:checked").val();
+  var schedule = $(".input-schedule:checked").val();
+  var clearing = $(".input-clearing:checked").val();
+  var fordeduct = $(".input-fordeduct:checked").val();
+  var borfapps = $(".input-borfapps:checked").val();
+  var manual = $(".input-manual:checked").val();
+  var dmpiraw = $(".input-dmpiraw:checked").val();
+  var deduction = $(".input-deduction:checked").val();
+  var deductdoc = $(".input-deductdoc:checked").val();
+  var paiddeduction = $(".input-paiddeduction:checked").val();
+  var payment = $(".input-payment:checked").val();
+  var rts = $(".input-rts:checked").val();
+  var pulloutdoc = $(".input-pulloutdoc:checked").val();
+  var report = $(".input-report:checked").val();
+  var system = $(".input-syssetting:checked").val();
+  var isActive = $(".input-active:checked").val();
+  $.ajax({
+    type: "POST",
+    url: "user/newuser.php",
+    data: {
+      username: username,
+      password: password,
+      fname: fname,
+      admin: admin,
+      semiadmin: semiadmin,
+      comp1: comp1,
+      comp2: comp2,
+      comp3: comp3,
+      comp4: comp4,
+      comp5: comp5,
+      comp6: comp6,
+      comp7: comp7,
+      comp8: comp8,
+      comp9: comp9,
+      comp10: comp10,
+      loc1: loc1,
+      loc2: loc2,
+      loc3: loc3,
+      loc4: loc4,
+      loc5: loc5,
+      loc6: loc6,
+      loc7: loc7,
+      loc8: loc8,
+      loc9: loc9,
+      loc10: loc10,
+      store: store,
+      inventory: inventory,
+      upload: upload,
+      importdop: importdop,
+      print: print,
+      schedule: schedule,
+      clearing: clearing,
+      manual: manual,
+      fordeduct: fordeduct,
+      borfapps: borfapps,
+      dmpiraw: dmpiraw,
+      deduction: deduction,
+      deductdoc: deductdoc,
+      paiddeduction: paiddeduction,
+      payment: payment,
+      rts: rts,
+      pulloutdoc: pulloutdoc,
+      report: report,
+      system: system,
+      active: isActive,
+    },
+    success: function () {
+      $(".span-notify-alert").html("User successfully added!").show();
+      $(".div-load-data").html("");
+      $(".span-notify-alert").css("background-color", "");
+      setTimeout(function () {
+        $(".span-notify-alert").fadeOut();
+      }, 2000);
+    },
+    error: function () {
+      alert("Error: Failed to update user.");
+    },
+  });
+  return false;
 }
+
 function UpdateUser() {
-  var _0x583f16 = _0x143b0a,
-    _0x45a1f2 = $(_0x583f16(0x188))["val"](),
-    _0x1f5ba7 = $(_0x583f16(0x113))[_0x583f16(0x141)](),
-    _0x5aabf4 = $(_0x583f16(0x14f))["val"](),
-    _0x37152e = $(_0x583f16(0x119))["val"](),
-    _0x781872 = $(".input-admin:checked")[_0x583f16(0x141)](),
-    _0x5ee540 = $(".input-semiadmin:checked")[_0x583f16(0x141)](),
-    _0xc8d77f = $(_0x583f16(0x11a))[_0x583f16(0x141)](),
-    _0x29e7f1 = $(".input-comp2:checked")["val"](),
-    _0x121256 = $(".input-comp3:checked")["val"](),
-    _0x424682 = $(_0x583f16(0x110))[_0x583f16(0x141)](),
-    _0x3ef5c1 = $(_0x583f16(0x144))[_0x583f16(0x141)](),
-    _0xb72af6 = $(".input-comp6:checked")[_0x583f16(0x141)](),
-    _0x9afafc = $(_0x583f16(0x136))[_0x583f16(0x141)](),
-    _0x32f9b0 = $(_0x583f16(0x161))[_0x583f16(0x141)](),
-    _0x37af22 = $(_0x583f16(0x13f))[_0x583f16(0x141)](),
-    _0x44d3f1 = $(_0x583f16(0x14c))[_0x583f16(0x141)](),
-    _0x3e700a = $(_0x583f16(0x131))[_0x583f16(0x141)](),
-    _0x3fc6ca = $(_0x583f16(0x146))[_0x583f16(0x141)](),
-    _0xddae9c = $(_0x583f16(0x186))[_0x583f16(0x141)](),
-    _0x53205e = $(_0x583f16(0x16b))[_0x583f16(0x141)](),
-    _0x4a4600 = $(_0x583f16(0x17c))[_0x583f16(0x141)](),
-    _0x15d9fe = $(_0x583f16(0x18f))["val"](),
-    _0xf72460 = $(_0x583f16(0x117))[_0x583f16(0x141)](),
-    _0x1a2185 = $(_0x583f16(0x10d))["val"](),
-    _0x534e33 = $(_0x583f16(0x135))[_0x583f16(0x141)](),
-    _0x179676 = $(".input-loc10:checked")[_0x583f16(0x141)](),
-    _0x17e939 = $(".input-store:checked")[_0x583f16(0x141)](),
-    _0x583f9e = $(_0x583f16(0x1ae))["val"](),
-    _0x5d2066 = $(_0x583f16(0x14d))[_0x583f16(0x141)](),
-    _0x158eae = $(_0x583f16(0x19d))["val"](),
-    _0x561274 = $(_0x583f16(0x172))[_0x583f16(0x141)](),
-    _0x1c9974 = $(_0x583f16(0x1a0))[_0x583f16(0x141)](),
-    _0x6723c3 = $(".input-clearing:checked")[_0x583f16(0x141)](),
-    _0x42457a = $(_0x583f16(0x19a))[_0x583f16(0x141)](),
-    _0x3831b8 = $(_0x583f16(0x1c2))[_0x583f16(0x141)](),
-    _0x6a3868 = $(_0x583f16(0x19b))[_0x583f16(0x141)](),
-    _0x159f86 = $(_0x583f16(0x1a3))[_0x583f16(0x141)](),
-    _0x507a0f = $(_0x583f16(0x1ba))[_0x583f16(0x141)](),
-    _0x2007fe = $(_0x583f16(0x171))[_0x583f16(0x141)](),
-    _0x41f3c3 = $(_0x583f16(0x180))["val"](),
-    _0x515c27 = $(".input-payment:checked")[_0x583f16(0x141)](),
-    _0x472424 = $(_0x583f16(0x147))[_0x583f16(0x141)](),
-    _0x39e170 = $(_0x583f16(0x1a6))["val"](),
-    _0x774f58 = $(_0x583f16(0x13a))[_0x583f16(0x141)](),
-    _0x445800 = $(_0x583f16(0x191))[_0x583f16(0x141)](),
-    _0x1e84b = $(_0x583f16(0x129))[_0x583f16(0x141)]();
-  return (
-    $["ajax"]({
-      type: _0x583f16(0x114),
-      url: 'user/newuser.php',
-      data: {
-        id: _0x45a1f2,
-        username: _0x1f5ba7,
-        password: _0x5aabf4,
-        fname: _0x37152e,
-        admin: _0x781872,
-        semiadmin: _0x5ee540,
-        comp1: _0xc8d77f,
-        comp2: _0x29e7f1,
-        comp3: _0x121256,
-        comp4: _0x424682,
-        comp5: _0x3ef5c1,
-        comp6: _0xb72af6,
-        comp7: _0x9afafc,
-        comp8: _0x32f9b0,
-        comp9: _0x37af22,
-        comp10: _0x44d3f1,
-        loc1: _0x3e700a,
-        loc2: _0x3fc6ca,
-        loc3: _0xddae9c,
-        loc4: _0x53205e,
-        loc5: _0x4a4600,
-        loc6: _0x15d9fe,
-        loc7: _0xf72460,
-        loc8: _0x1a2185,
-        loc9: _0x534e33,
-        loc10: _0x179676,
-        store: _0x17e939,
-        inventory: _0x583f9e,
-        upload: _0x5d2066,
-        importdop: _0x158eae,
-        print: _0x561274,
-        schedule: _0x1c9974,
-        clearing: _0x6723c3,
-        manual: _0x6a3868,
-        fordeduct: _0x42457a,
-        borfapps: _0x3831b8,
-        dmpiraw: _0x159f86,
-        deduction: _0x507a0f,
-        deductdoc: _0x2007fe,
-        paiddeduction: _0x41f3c3,
-        payment: _0x515c27,
-        rts: _0x472424,
-        pulloutdoc: _0x39e170,
-        report: _0x774f58,
-        system: _0x445800,
-        active: _0x1e84b,
-      },
-      success: function () {
-        var _0x38da39 = _0x583f16;
-        $(_0x38da39(0x179))["html"](_0x38da39(0x148)),
-          $(_0x38da39(0x179))[_0x38da39(0x156)](),
-          $(".div-load-data")["html"](""),
-          $(_0x38da39(0x1b2))[_0x38da39(0x1b7)]("background-Color", ""),
-          setTimeout(function () {
-            var _0x16f040 = _0x38da39;
-            $(".span-notify-alert")[_0x16f040(0x126)]();
-          }, 0x7d0);
-      },
-    }),
-    ![]
-  );
+  var pageId = $(".select-user").val();
+  var username = $(".input-username").val();
+  var password = $(".input-password").val();
+  var fname = $(".input-fname").val();
+  var admin = $(".input-admin:checked").val();
+  var semiadmin = $(".input-semiadmin:checked").val();
+  var comp1 = $(".input-comp1:checked").val();
+  var comp2 = $(".input-comp2:checked").val();
+  var comp3 = $(".input-comp3:checked").val();
+  var comp4 = $(".input-comp4:checked").val();
+  var comp5 = $(".input-comp5:checked").val();
+  var comp6 = $(".input-comp6:checked").val();
+  var comp7 = $(".input-comp7:checked").val();
+  var comp8 = $(".input-comp8:checked").val();
+  var comp9 = $(".input-comp9:checked").val();
+  var comp10 = $(".input-comp10:checked").val();
+  var loc1 = $(".input-loc1:checked").val();
+  var loc2 = $(".input-loc2:checked").val();
+  var loc3 = $(".input-loc3:checked").val();
+  var loc4 = $(".input-loc4:checked").val();
+  var loc5 = $(".input-loc5:checked").val();
+  var loc6 = $(".input-loc6:checked").val();
+  var loc7 = $(".input-loc7:checked").val();
+  var loc8 = $(".input-loc8:checked").val();
+  var loc9 = $(".input-loc9:checked").val();
+  var loc10 = $(".input-loc10:checked").val();
+  var store = $(".input-store:checked").val();
+  var inventory = $(".input-inventory:checked").val();
+  var upload = $(".input-upload:checked").val();
+  var imp = $(".input-import:checked").val();
+  var importdop = $(".input-importdop:checked").val();
+  var print = $(".input-print:checked").val();
+  var schedule = $(".input-schedule:checked").val();
+  var clearing = $(".input-clearing:checked").val();
+  var fordeduct = $(".input-fordeduct:checked").val();
+  var borfapps = $(".input-borfapps:checked").val();
+  var manual = $(".input-manual:checked").val();
+  var dmpiraw = $(".input-dmpiraw:checked").val();
+  var deduction = $(".input-deduction:checked").val();
+  var deductdoc = $(".input-deductdoc:checked").val();
+  var paiddeduction = $(".input-paiddeduction:checked").val();
+  var payment = $(".input-payment:checked").val();
+  var rts = $(".input-rts:checked").val();
+  var pulloutdoc = $(".input-pulloutdoc:checked").val();
+  var report = $(".input-report:checked").val();
+  var system = $(".input-system:checked").val();
+  var isActive = $(".input-active:checked").val();
+  $.ajax({
+    type: "POST",
+    url: "user/updateuser.php",
+    data: {
+      id: pageId,
+      username: username,
+      password: password,
+      fname: fname,
+      admin: admin,
+      semiadmin: semiadmin,
+      comp1: comp1,
+      comp2: comp2,
+      comp3: comp3,
+      comp4: comp4,
+      comp5: comp5,
+      comp6: comp6,
+      comp7: comp7,
+      comp8: comp8,
+      comp9: comp9,
+      comp10: comp10,
+      loc1: loc1,
+      loc2: loc2,
+      loc3: loc3,
+      loc4: loc4,
+      loc5: loc5,
+      loc6: loc6,
+      loc7: loc7,
+      loc8: loc8,
+      loc9: loc9,
+      loc10: loc10,
+      store: store,
+      inventory: inventory,
+      upload: upload,
+      import: imp,
+      importdop: importdop,
+      print: print,
+      schedule: schedule,
+      clearing: clearing,
+      manual: manual,
+      fordeduct: fordeduct,
+      borfapps: borfapps,
+      dmpiraw: dmpiraw,
+      deduction: deduction,
+      deductdoc: deductdoc,
+      paiddeduction: paiddeduction,
+      payment: payment,
+      rts: rts,
+      pulloutdoc: pulloutdoc,
+      report: report,
+      system: system,
+      active: isActive,
+    },
+    success: function () {
+      $(".span-notify-alert").html("User updated successfully.").show();
+      $(".div-load-data").html("");
+      $(".span-notify-alert").css("background-color", "");
+      setTimeout(function () {
+        $(".span-notify-alert").fadeOut();
+      }, 2000);
+    },
+    error: function () {
+      alert("Error: Failed to update user.");
+    },
+  });
+
+  return false;
 }
+
 function _0x157f(_0x1eb003, _0x1e84c6) {
   var _0x3b6337 = _0x3b63();
   return (
